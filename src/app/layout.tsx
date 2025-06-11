dbf5806d3098bcc57ecfe536e360008c9eddab21
Victor Liang <victorl1725@gmail.com>
2025-06-11 02:39:52 -0700

    added analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next";
-// Remove Inter font import if it's not the intended default
-// import { Inter } from "next/font/google";
+import { Analytics } from "@vercel/analytics/next"
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
 import Navbar from "@/components/Navbar";
@@ -42,6 +41,7 @@ export default function RootLayout({
         {/* Change strategy to 'afterInteractive' */}
         <Script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js" strategy="afterInteractive" />
         <Script src="https://cdnjs.cloudflare.com/ajax/libs/mathquill/0.10.1/mathquill.js" strategy="afterInteractive" />
+        <Analytics />
       </body>
     </html>
   );
